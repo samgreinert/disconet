224ddecf9f8a000c42614e6489230bde01160990
Samantha Greinert <50837724+samgreinert@users.noreply.github.com>
2025-01-17 15:53:43 -0500

    sdf

--- templates/themes/index/info.php
+++ templates/themes/index/info.php
@@ -1,69 +1,69 @@
 <?php
 	$theme = Array();
-	
+
 	// Theme name
 	$theme['name'] = 'Index';
 	// Description (you can use Tinyboard markup here)
 	$theme['description'] = 'Show a homepage';
 	$theme['version'] = 'v1.0';
-	
-	// Theme configuration	
+
+	// Theme configuration
 	$theme['config'] = Array();
-	
+
 	$theme['config'][] = Array(
 		'title' => 'Icon',
 		'name' => 'icon',
 		'type' => 'text',
-		'default' => '../templates/themes/index/hikichanIcon.png',
+		'default' => '../templates/themes/index/disco-net.png',
 		'size' => 50
 	);
-	
+
 	$theme['config'][] = Array(
 		'title' => 'Title',
 		'name' => 'title',
 		'type' => 'text',
-		'default' => 'Welcome to my Image Board',
+		'default' => 'Welcome to DiscoNet!',
 		'size' => 50
 	);
-	
+
 	$theme['config'][] = Array(
 		'title' => 'Subtitle',
 		'name' => 'subtitle',
 		'type' => 'text',
-		'default' => 'What is chaos for the fly is normal for the spider.',
+		'default' => 'OUR MODERATION IS THAT YOU\'RE IN ARM\'S REACH. SO MODERATE YOURSELF.',
 		'size' => 50
 	);
-	
+
 	$theme['config'][] = Array(
 		'title' => 'Description',
 		'name' => 'description',
 		'type' => 'textarea',
-		'default' => 'Short description for your website.'
+		'default' => 'Social media sucks, let\'s build something better'
 	);
-	
+
 		$theme['config'][] = Array(
 		'title' => 'Image of the now.',
 		'name' => 'imageofnow',
 		'type' => 'text',
-		'default' => '../templates/themes/index/hotweels.jpg',
+		'default' => '../templates/themes/index/disco-net.png',
 		'size' => 50
 	);
-	
+
 		$theme['config'][] = Array(
 		'title' => 'Quote of the now.',
 		'name' => 'quoteofnow',
 		'type' => 'textarea',
-		'default' => '"Great minds discuss ideas; average minds discuss events; small minds discuss people." - QUOTE'
+		'default' => ''
 	);
-	
+
 		$theme['config'][] = Array(
 		'title' => 'Video of the Now',
 		'name' => 'videoofnow',
 		'type' => 'text',
-		'default' => 'https://www.youtube.com/embed/YbaTur4A1OU',
+		'default' => '',
 		'size' => 50
 	);
-	
+
 	$theme['config'][] = Array(
 		'title' => '# of recent entries',
 		'name' => 'no_recent',
@@ -72,14 +72,14 @@
 		'size' => 3,
 		'comment' => '(number of recent news entries to display; "0" is infinite)'
 	);
-	
+
 	$theme['config'][] = Array(
 		'title' => 'Excluded boards',
 		'name' => 'exclude',
 		'type' => 'text',
 		'comment' => '(space seperated)'
 	);
-	
+
 	$theme['config'][] = Array(
 		'title' => '# of recent images',
 		'name' => 'limit_images',
@@ -87,7 +87,7 @@
 		'default' => '15',
 		'comment' => '(maximum images to display)'
 	);
-	
+
 	$theme['config'][] = Array(
 		'title' => '# of recent posts',
 		'name' => 'limit_posts',
@@ -95,7 +95,7 @@
 		'default' => '30',
 		'comment' => '(maximum posts to display)'
 	);
-	
+
 	$theme['config'][] = Array(
 		'title' => 'HTML file',
 		'name' => 'html',
@@ -103,7 +103,7 @@
 		'default' => 'index.html',
 		'comment' => '(eg. "index.html")'
 	);
-	
+
 	$theme['config'][] = Array(
 		'title' => 'CSS file',
 		'name' => 'css',
@@ -119,7 +119,7 @@
 		'default' => 'index.css',
 		'comment' => '(eg. "index.css" - see templates/themes/index for details)'
 	);
-	
+
 	// Unique function name for building everything
 	$theme['build_function'] = 'index_build';
 	$theme['install_callback'] = 'index_install';
@@ -134,4 +134,3 @@
 				return Array(false, '<strong>' . utf8tohtml($settings['no_recent']) . '</strong> is not a non-negative integer.');
 		}
 	}
-	
